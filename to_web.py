2348548f75c2add043a839ac4853c297306e313a
bertez@gmail.com <berto@senbei.(none)>
2013-04-13 18:09:06 +0200

    pep8

--- to_web.py
+++ to_web.py
@@ -1,38 +1,46 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys, os, time
+import sys
+import os
+import time
 import shlex
 import subprocess as sub
 
+
 class BatchProcess(object):
-    """This class processes a list of videos to video formats usable by modern browsers (mp4 and ogv)"""
+    """This class processes a list of videos to video formats usable by modern
+    browsers (mp4 and ogv)"""
 
     __author__ = 'Berto Yáñez'
     __version__ = '0.1'
 
     encodings = {
-            'mp4' : ['ffmpeg -y -i {0} -pass 1 -vcodec libx264 -vpre fast_firstpass -b {3}k -bt {4}k -threads 4 {2}-f rawvideo -an -y /dev/null','ffmpeg -y -i {0} -pass 2 -acodec libfaac -ab {4}k -ac 2 -vcodec libx264 -vpre slow -b {3}k -bt {3}k -threads 4 {2}{1}'],
-            'ogv' : ['ffmpeg2theora -V {3} -A {4} {2}--two-pass {0} -o {1}']
-            }
+        'mp4': ['ffmpeg -y -i {0} -pass 1 -vcodec libx264 -vpre \
+                fast_firstpass -b {3}k -bt {4}k -threads 4 {2}-f rawvideo \
+                -an -y /dev/null', 'ffmpeg -y -i {0} -pass 2 -acodec libfaac \
+                -ab {4}k -ac 2 -vcodec libx264 -vpre slow -b {3}k -bt {3}k \
+                -threads 4 {2}{1}'],
+        'ogv': ['ffmpeg2theora -V {3} -A {4} {2}--two-pass {0} -o {1}']
+    }
 
     wh_patterns = {
-            'mp4' : '-s {0}x{1} ',
-            'ogv' : '-x {0} -y {1} '
-            }
+        'mp4': '-s {0}x{1} ',
+        'ogv': '-x {0} -y {1} '
+    }
 
     localDir = os.getcwd()
     errors = {}
     logs = {}
 
-    def __init__(self, arguments, output_dir, width, height, disable_mp4, disable_ogv, videobr, audiobr):
+    def __init__(self, arguments, output_dir, width, height, disable_mp4,
+                 disable_ogv, videobr, audiobr):
 
         self.filelist = self.create_file_list(arguments)
         self.target_dir = output_dir
 
         self.width = width
         self.height = height
-
 
         self.disable_mp4 = disable_mp4
         self.disable_ogv = disable_ogv
@@ -42,9 +50,9 @@ class BatchProcess(object):
 
         self.outputdir = os.path.join(self.localDir, self.target_dir)
 
-        self.logfile = self.outputdir + os.sep + str(time.time()) +'_job.log'
-        self.errorfile = self.outputdir + os.sep + str(time.time()) +'_errors_job.log'
-
+        self.logfile = self.outputdir + os.sep + str(time.time()) + '_job.log'
+        self.errorfile = self.outputdir + os.sep + \
+            str(time.time()) + '_errors_job.log'
 
         if not os.path.exists(self.outputdir):
             os.mkdir(self.outputdir)
@@ -77,38 +85,44 @@ class BatchProcess(object):
         errorfile = open(self.errorfile, 'w')
 
         for video in self.filelist:
-            self.errors[video] =  []
+            self.errors[video] = []
             self.logs[video] = []
-
 
             video_output = os.path.splitext(os.path.basename(video))[0]
             print 'Processing', video
-            print '---'*10
+            print '---' * 10
 
             self.logs[video].append('Starting process of %s' % video)
 
             for finalformat, command in self.encodings.items():
                 print 'Creating %s file. Be patient.' % finalformat
 
-                final_file = self.outputdir + os.sep + video_output + '.' + finalformat
+                final_file = self.outputdir + os.sep + \
+                    video_output + '.' + finalformat
                 for cmd in command:
 
-                    wh_string = "" if self.width is None or self.height is None else self.wh_patterns[finalformat].format(self.width, self.height) + ' '
-                    subp = cmd.format(video, final_file, wh_string , self.videobr, self.audiobr)
+                    wh_string = "" if self.width is None or \
+                        self.height is None else self.wh_patterns[
+                            finalformat].format(self.width, self.height) + ' '
+
+                    subp = cmd.format(
+                        video, final_file, wh_string, self.videobr,
+                        self.audiobr)
 
                     self.logs[video].append(subp)
 
                     try:
                         pipe = sub.Popen(shlex.split(subp), stderr=sub.PIPE)
                     except OSError:
-                        self.errors[video].append('System error. Check dependencies.')
+                        self.errors[video].append(
+                            'System error. Check dependencies.')
                     except ValueError:
                         self.errors[video].append('FFmpeg error. Sorry')
 
                     self.logs[video].extend(pipe.stderr.readlines())
 
             logfile.writelines(self.logs[video])
-            logfile.write('\n'*3)
+            logfile.write('\n' * 3)
             errorfile.writelines(self.errors[video])
 
         if len(self.errors[video]) == 0:
@@ -116,12 +130,15 @@ class BatchProcess(object):
         else:
             print "\nThere were errors:\n"
             for error in self.errors[video]:
-                print error+'\n'
+                print error + '\n'
         logfile.close()
         errorfile.close()
 
     def __repr__(self):
-        resize_info = 'The videos will not be resized' if self.width is None or self.height is None else 'The output video(s) will be this size: {0}x{1}'.format(self.width, self.height)
+        resize_info = 'The videos will not be resized' if self.width is None \
+            or self.height is None else 'The output video(s) will be this \
+                size: {0}x{1}'.format(
+            self.width, self.height)
 
         batch_info = """
 
@@ -131,7 +148,8 @@ class BatchProcess(object):
     Audio bitrate will be: {3}kbps
     You'll find the compressed videos here: {4}
 
-        """.format(str(self.filelist), resize_info, self.videobr, self.audiobr, self.outputdir)
+        """.format(str(self.filelist), resize_info, self.videobr,
+                   self.audiobr, self.outputdir)
 
         return batch_info
 
@@ -140,44 +158,55 @@ if __name__ == '__main__':
 
     import argparse
 
-    parser = argparse.ArgumentParser(description="Example usage: to_web.py --videos video1.avi video2.avi --width 960 --height 540 --output-dir upload")
+    parser = argparse.ArgumentParser(
+        description="Example usage: to_web.py --videos video1.avi video2.avi \
+        --width 960 --height 540 --output-dir upload")
 
     group = parser.add_argument_group('resize')
 
-    parser.add_argument('--videos', action='store', dest='videos', default=[], required = True,
-            help='Video to process. You can add multiple.', nargs='+'
-            )
+    parser.add_argument(
+        '--videos', action='store', dest='videos', default=[], required=True,
+        help='Video to process. You can add multiple.', nargs='+'
+    )
 
-    parser.add_argument('--output-dir', action='store', dest='output_dir', default='videos_to_web',
-            help='Output directory. Path. Default: videos_to_web')
+    parser.add_argument(
+        '--output-dir', action='store', dest='output_dir',
+        default='videos_to_web', help='Output directory. \
+        Path. Default: videos_to_web')
 
     group.add_argument('--width', action='store', dest='width', default=None,
-            help='Output video width. Number.', type=int)
+                       help='Output video width. Number.', type=int)
 
     group.add_argument('--height', action='store', dest='height', default=None,
-            help='Output video height. Number.', type=int)
+                       help='Output video height. Number.', type=int)
 
-    parser.add_argument('--video-bitrate', action='store', dest='videobr', default=1500,
-            help='Output video bitrate. Number. Default: 1500', type=int)
+    parser.add_argument(
+        '--video-bitrate', action='store', dest='videobr', default=1500,
+        help='Output video bitrate. Number. Default: 1500', type=int)
 
-    parser.add_argument('--audio-bitrate', action='store', dest='audiobr', default=128,
-            help='Output audio bitrate. Number. Default: 128', type=int)
+    parser.add_argument(
+        '--audio-bitrate', action='store', dest='audiobr', default=128,
+        help='Output audio bitrate. Number. Default: 128', type=int)
 
     parser.add_argument('--disable-mp4', action='store_true', default=False,
-            dest='disable_mp4',
-            help='Do not generate the mp4 file.')
+                        dest='disable_mp4',
+                        help='Do not generate the mp4 file.')
 
     parser.add_argument('--disable-ogv', action='store_true', default=False,
-            dest='disable_ogv',
-            help='Do not generate the ogv file')
+                        dest='disable_ogv',
+                        help='Do not generate the ogv file')
 
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1 by @bertez')
+    parser.add_argument(
+        '--version', action='version', version='%(prog)s 0.1 by @bertez')
 
     results = parser.parse_args()
 
-    #sys.exit(results)
+    # sys.exit(results)
 
-    my_batch = BatchProcess(results.videos, results.output_dir, results.width, results.height, results.disable_mp4, results.disable_ogv, results.videobr, results.audiobr)
+    my_batch = BatchProcess(
+        results.videos, results.output_dir, results.width, results.height,
+        results.disable_mp4, results.disable_ogv, results.videobr,
+        results.audiobr)
 
     print 'Batch process info: ', my_batch
 
